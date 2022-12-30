bb598adc887c9b1b521339840385a60cbff5722a
benjamin chukwudi <73422820+benjaminugwuanyi@users.noreply.github.com>
2022-12-30 07:25:24 +0100

    all

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword33 = 'mongodb://localhost:27017/mern2';
+dbPassword = 'mongodb+srv://Exora:HelloToday12@cluster0.rdyjgpm.mongodb.net/ExoraUsers'
+module.exports = {
+    mongoURI: dbPassword
+};
